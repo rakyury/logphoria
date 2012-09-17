11da7c850d2879b33b7de6e3d936b5a9fcb4a6f1
Rak Yury <rak.yury@gmail.com>
2012-09-17 15:32:22 +0200

    removed unused files

--- php/logEvent.php
+++
@@ -1,8 +0,0 @@
-<?php require("logphoria.php");
-// make sure the CAcerts folder is in the same path as these .php files as well!
-
-$logphoria = new Logphoria('eddfab7a-a992-0311-80f8-bbc22b69a8a1');
-
-$logphoria->log('INFO', 'Test message from php', array('prop1' => 'value1', 'prop2' => 2));
-
-?>
